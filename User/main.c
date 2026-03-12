e2172c19b8ea370fbc2256254f9e0f10078dc743
QQ <2970769638@qq.com>
2026-03-12 15:58:39 +0800

    环境初始化配置，LED配置，OLED、Delay函数初始化配置

--- User/main.c
+++ User/main.c
@@ -1,10 +1,12 @@
 #include "stm32f10x.h"                  // Device header
+#include "LED.h"
 
 int main(void)
 {
+	LED_Init();
 	while(1)
 	{
-	
+		GPIO_SetBits(GPIOB,GPIO_Pin_0|GPIO_Pin_1);
 	}
 }
 
